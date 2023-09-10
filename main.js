059634abd0183c3d0e2449b3d67bc86b664efde8
55555hayden <122558334+55555hayden@users.noreply.github.com>
2023-09-10 13:07:12 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+function tocaSom1 (){
+    document.querySelector('#som_tecla_patati').play();
+}
+function tocaSom2(){
+    document.querySelector('#som_tecla_mygod').play();
+}
+function tocaSom3(){
+    document.querySelector('#som_tecla_elefezdenovo').play();
+}
+function tocaSom4(){
+    document.querySelector('#som_tecla_finalcomico').play();
+}
+function tocaSom5(){
+    document.querySelector('#som_tecla_homemmacaco').play();
+}
+function tocaSom6(){
+    document.querySelector('#som_tecla_risadadeladrao').play();
+}
+function tocaSom7(){
+    document.querySelector('#som_tecla_yametekudasai').play();
+}
+function tocaSom8(){
+    document.querySelector('#som_tecla_deliciamickey').play();
+}
+function tocaSom9(){
+    document.querySelector('#som_tecla_sad').play();
+}
+
+
+document.querySelector('.tecla_1').onclick = tocaSom1;
+document.querySelector('.tecla_2').onclick = tocaSom2;
+document.querySelector('.tecla_3').onclick = tocaSom3;
+document.querySelector('.tecla_4').onclick = tocaSom4;
+document.querySelector('.tecla_5').onclick = tocaSom5;
+document.querySelector('.tecla_6').onclick = tocaSom6;   
+document.querySelector('.tecla_7').onclick = tocaSom7;
+document.querySelector('.tecla_8').onclick = tocaSom8;
+document.querySelector('.tecla_9').onclick = tocaSom9; 
