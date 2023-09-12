b39b96e744cee2e3b91f81513bbc36e22882eede
55555hayden <122558334+55555hayden@users.noreply.github.com>
2023-09-12 09:15:11 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,38 +1,39 @@
-function tocaSom1 (){
-    document.querySelector('#som_tecla_patati').play();
-}
-function tocaSom2(){
-    document.querySelector('#som_tecla_mygod').play();
-}
-function tocaSom3(){
-    document.querySelector('#som_tecla_elefezdenovo').play();
-}
-function tocaSom4(){
-    document.querySelector('#som_tecla_finalcomico').play();
-}
-function tocaSom5(){
-    document.querySelector('#som_tecla_homemmacaco').play();
-}
-function tocaSom6(){
-    document.querySelector('#som_tecla_risadadeladrao').play();
-}
-function tocaSom7(){
-    document.querySelector('#som_tecla_yametekudasai').play();
-}
-function tocaSom8(){
-    document.querySelector('#som_tecla_deliciamickey').play();
-}
-function tocaSom9(){
-    document.querySelector('#som_tecla_sad').play();
+function tocaSom (seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);
+
+    if (elemento && elemento.localName === 'audio') {
+        elemento.play();
+    }
+    else {
+        //alert('Elemento não encontrado');
+        console.log('Elemento não encontrado ou seletor inválido');
+    }
+
 }
 
+const listaDeTeclas = document.querySelectorAll('.tecla');
 
-document.querySelector('.tecla_1').onclick = tocaSom1;
-document.querySelector('.tecla_2').onclick = tocaSom2;
-document.querySelector('.tecla_3').onclick = tocaSom3;
-document.querySelector('.tecla_4').onclick = tocaSom4;
-document.querySelector('.tecla_5').onclick = tocaSom5;
-document.querySelector('.tecla_6').onclick = tocaSom6;   
-document.querySelector('.tecla_7').onclick = tocaSom7;
-document.querySelector('.tecla_8').onclick = tocaSom8;
-document.querySelector('.tecla_9').onclick = tocaSom9; 
+//para
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`; //template string
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
+
+    tecla.onkeydown = function (evento) {
+
+        if (evento.code === 'Space' || evento.code === 'Enter') {
+            tecla.classList.add('ativa');
+        }
+
+    }
+
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
+
+}
